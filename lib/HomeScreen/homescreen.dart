ee2c22e1600d481a713afd6e9df79b18ea254974
Jayanth Naidu <71186065+noobguy77@users.noreply.github.com>
2021-05-30 19:18:53 +0530

    Update homescreen.dart

--- lib/HomeScreen/homescreen.dart
+++ lib/HomeScreen/homescreen.dart
@@ -1,152 +1,186 @@
-import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/Events/home.dart';
+import 'package:untitled/Events/Eventpage/home.dart';
+import 'package:untitled/Screens/Developers.dart';
+import 'package:untitled/Screens/Organisers.dart';
+import 'package:untitled/Screens/about.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+class HomeScreen extends StatelessWidget {
+  List data = [
+    {"color": Color(0xffe53935)},
+    {"color": Color(0xff96da45)},
+    {"color": Color(0xff7a54ff)},
+    {"color": Color(0xff2ac3ff)},
+  ];
 
-class _HomePageState extends State<HomePage> {
+  List data1 = [
+    {"event": "Events"},
+    {"event": "Organisers"},
+    {"event": "Developers"},
+    {"event": " About"},
+  ];
+  List icons = [
+    {"icon": Icons.event},
+    {"icon": Icons.people},
+    {"icon": Icons.people},
+    {"icon": Icons.help},
+  ];
+  List events = [
+    {"event": new TabView()},
+    {"event": new Organisers()},
+    {"event": new Developers()},
+    {
+      "event": new About(),
+    },
+  ];
+  final colorwhite = Colors.white;
   @override
   Widget build(BuildContext context) {
-    Widget image_curousel = new Container(
-      height: 230.0,
-      child: new Carousel(
-        boxFit: BoxFit.cover,
-        images: [
-          AssetImage('images/college.jpg'),
+    var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
+    return Scaffold(
+      //bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            // Here the height of the container is 45% of our total height
+            height: size.height * .38,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.center,
+                image: AssetImage("images/college.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                  ),
+                  Text(
+                    "\n\n\n",
+                    style: Theme.of(context)
+                        .textTheme
+                        .display1!
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  SearchBar(),
+                  Expanded(
+                    child: GridView.builder(
+                      itemCount: data.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.0,
+                        crossAxisSpacing: .1,
+                        mainAxisSpacing: 10,
+                        // crossAxisSpacing: 10
+                      ),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 0.5),
+                          child: InkWell(
+                            onTap: () => Navigator.of(context).push(
+                                new MaterialPageRoute(
+
+                                    //passing values to prod
+                                    builder: (context) =>
+                                        events[index]["event"])),
+                            child: Card(
+                              color: data[index]["color"],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(0.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "        ",
+                                          style: TextStyle(
+                                              color: colorwhite, fontSize: 16),
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.all(20),
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(30),
+                                                  bottomLeft:
+                                                      Radius.circular(30),
+                                                  bottomRight:
+                                                      Radius.circular(30)),
+                                              color: Color.fromRGBO(
+                                                  255, 255, 255, 0.38)),
+                                          child: Icon(
+                                            icons[index]["icon"],
+                                            color: colorwhite,
+                                            size: 24,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    Container(
+                                        alignment: Alignment.topLeft,
+                                        padding: EdgeInsets.only(
+                                            left: 10, bottom: 5),
+                                        child: Text(
+                                          data1[index]["event"],
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 24,
+                                            color: colorwhite,
+                                          ),
+                                        )),
+                                    Container(
+                                      alignment: Alignment.bottomRight,
+                                      padding: EdgeInsets.only(right: 10),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
-        autoplay: true,
-        animationCurve: Curves.fastOutSlowIn,
-        animationDuration: Duration(seconds: 5),
-        dotSize: 0.0,
-        indicatorBgPadding: 0.0,
       ),
     );
+  }
+}
 
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('WELCOME TO IARE', style: TextStyle(color: Colors.white)),
-          elevation: 0.0,
-          titleSpacing: 10.0,
-          centerTitle: true,
-          // leading: InkWell(
-          //   onTap: () {},
-          //   child: Icon(Icons.water_damage,
-          //       color: Colors.white),
-          // ),
-        ),
-        drawer: new Drawer(
-            child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: Text("Aravind Reddy"),
-              accountEmail: Text("armr@gmail.com"),
-              currentAccountPicture: GestureDetector(
-                child: new CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, color: Colors.white),
-                ),
-              ),
-              decoration: new BoxDecoration(color: Colors.red),
-            ),
-            InkWell(
-                onTap: () => Navigator.of(context).pop(
-                    new MaterialPageRoute(builder: (context) => HomePage())),
-                child: ListTile(
-                  title: Text("Home Page"),
-                  leading: Icon(Icons.home, color: Colors.red),
-                )),
-            InkWell(
-                onTap: () => Navigator.of(context).push(
-                    new MaterialPageRoute(builder: (context) => TabView())),
-                child: ListTile(
-                  title: Text("My Registrations"),
-                  leading: Icon(Icons.shopping_basket, color: Colors.red),
-                )),
-            InkWell(
-                onTap: () => Navigator.of(context).push(
-                    new MaterialPageRoute(builder: (context) => TabView())),
-                child: ListTile(
-                  title: Text("Payments"),
-                  leading: Icon(Icons.payment, color: Colors.red),
-                )),
-            InkWell(
-                onTap: () => Navigator.of(context).push(
-                    new MaterialPageRoute(builder: (context) => TabView())),
-                child: ListTile(
-                  title: Text("Intrested Events"),
-                  leading: Icon(Icons.favorite, color: Colors.red),
-                )),
-            Divider(),
-            InkWell(
-                onTap: () => Navigator.of(context).push(
-                    new MaterialPageRoute(builder: (context) => TabView())),
-                child: ListTile(
-                  title: Text("Developers"),
-                  leading: Icon(Icons.person, color: Colors.red),
-                )),
-            InkWell(
-                onTap: () => Navigator.of(context).push(
-                    new MaterialPageRoute(builder: (context) => TabView())),
-                child: ListTile(
-                  title: Text("About Event"),
-                  leading: Icon(
-                    Icons.help,
-                    color: Colors.red,
-                  ),
-                ))
-          ],
-        )),
-        body: new ListView(children: <Widget>[
-          image_curousel,
-          new Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: new Text(
-              'Hai,',
-              style: TextStyle(fontSize: 40),
-            ),
-          ),
-          new Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: new Text(
-              'Aravind Reddy M',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            height: 100.0,
-            width: 50,
-            margin: EdgeInsets.all(20),
-            // ignore: deprecated_member_use
-            child: RaisedButton(
-              onPressed: () => Navigator.of(context).push(
-                  new MaterialPageRoute(builder: (context) => new TabView())),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(80.0)),
-              padding: EdgeInsets.all(0.0),
-              child: Ink(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xff374ABE), Color(0xff64B6FF)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(30.0)),
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "TECH FEST\n2021",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 30),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ]));
+const kBackgroundColor = Color(0xFFF8F8F8);
+const kActiveIconColor = Color(0xFFE68342);
+const kTextColor = Color(0xFF222B45);
+const kBlueLightColor = Color(0xFFC7B8F5);
+const kBlueColor = Color(0xFF817DC0);
+const kShadowColor = Color(0xFFE6E6E6);
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 30),
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(29.5),
+      ),
+    );
   }
 }
