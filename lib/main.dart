0fcd58414c9983b60d42c16c1b9716dd568790c8
aravindreddymokireddy <aravindreddy.mokireddy@gmail.com>
2021-06-20 00:02:33 +0530

    Revert "Revert "Merge branch 'master' of https://github.com/aravindreddymokireddy/Mr.X""
    This reverts commit 56d0b71fcd27c266a23c94cfda8a47f64c274709.

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,10 @@ import 'package:flutter/material.dart';
 import 'firestore/open_screen.dart';
 
 void main() {
-  runApp(IAREFest());
+  runApp(MyApp());
 }
 
-class IAREFest extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
